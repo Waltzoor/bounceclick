f2819d6ee9999b2afe635a7828598c1917475909
Waltzoor <valttiw@gmail.com>
2024-10-02 16:58:58 +0300

    Ghost luokka luotu

--- public/script.js
+++ public/script.js
@@ -16,9 +16,28 @@ function setup(){
 function draw(){
     background("blue")
     draw_paddle();
-    image(ghostPNG,0,0,64,64)
 }
 
 function draw_paddle(){
     rect(mouseX,paddleYPosition,paddleWidth,paddleHeight);
+}
+
+
+class Ghost{
+    constructor(){
+        this.x = 0;
+        this.y = 0;
+        this.width = 30
+        this.height = 30
+        this.xSpeed = 1;
+        this.ySpeed = -1;
+        this.gravity = 0.05;
+    }
+
+    Move(){
+        
+        image(ghostPNG,this.x,this.y,this.width,this.height)
+
+    }
+
 }
